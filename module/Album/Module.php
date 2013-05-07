b76045de1dda9f20d298c015ef3cb3b0df64f1be
Evolic <evolic@interia.pl>
2013-05-07 21:02:39 +0200

    - Code cleanup

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -2,11 +2,6 @@
 
 namespace Album;
 
-use Album\Model\Album;
-use Album\Model\AlbumTable;
-use Zend\Db\ResultSet\ResultSet;
-use Zend\Db\TableGateway\TableGateway;
-
 class Module
 {
     public function getAutoloaderConfig()
@@ -27,17 +22,6 @@ class Module
     {
         return array(
             'factories' => array(
-                'Album\Model\AlbumTable' =>  function($sm) {
-                    $tableGateway = $sm->get('AlbumTableGateway');
-                    $table = new AlbumTable($tableGateway);
-                    return $table;
-                },
-                'AlbumTableGateway' => function ($sm) {
-                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
-                    $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Album());
-                    return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
-                },
             ),
         );
     }
